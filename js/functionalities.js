43bc0c828a3ff23d62faf4119adcde6ef8b01ed1
Erikkui <erik.kuitunen@outlook.com>
2024-09-23 16:01:11 +0300

    Completed week 03 task 1

--- js/functionalities.js
+++ js/functionalities.js
@@ -1,95 +1,42 @@
 
-initializeCode();
 
-function initializeCode() {
 
-    // TODO add table data to Arrays for easier manipulation?
+async function generateTable() {
 
-    const addEntryButton = document.getElementById("submit-data")
-    addEntryButton.addEventListener("click", entryButtonClick )
+    const municipality_url = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
+    const dataTable = document.getElementById("data-table")
+
+    // Fetching JSON
+    const municipalityPromise = await fetch( municipality_url )
+    const municipalityJSON = await municipalityPromise.json()
+
+    // Extracting populations and municipalities from JSON
+    const municipalitiesArray = Object.values( municipalityJSON.dataset.dimension.Alue.category.label )
+    const populations = municipalityJSON.dataset.value
+
+    console.log(populations[2]);
+
+    // Adding rows to the table
+    // for (let ii = 0; ii < populations.length; ii++ ) {
+    //     const row = document.createElement( "tr" )
+    //     const townCell = document.createElement( "td" )
+    //     const populationCell = document.createElement( "td" )
+
+    addTableRows( dataTable, municipalitiesArray, populations )
+
+}
+
+function addTableRows( table, data1, data2 ) {
+
+    for (let ii = 0; ii < data1.length; ii++ ) {
+        let row = table.insertRow()
+        let municCell = row.insertCell()
+        let populCell = row.insertCell()
+
+        municCell.appendChild( document.createTextNode( data1[ii] ) )
+        populCell.appendChild( document.createTextNode( data2[ii] ) )
         
-    const emptyTableButton = document.getElementById("empty-table")
-    emptyTableButton.addEventListener("click", emptyListClick )
-}
-
-
-function entryButtonClick() {
-
-    const userTable = document.getElementById( "users-table" )
-    const newUsername = document.getElementById("input-username" );
-    const newEmail = document.getElementById( "input-email" );
-    const newIsAdmin = document.getElementById("input-admin" );
-
-    // Check if username exists
-    userExists = checkExistingUsername( newUsername.value, userTable )
-
-    // Edit existing name
-    if (userExists[0]) {
-        row = userExists[1];
-        data = userTable.rows[ row ]
-        data.cells[1].innerText = newEmail.value
-        if (newIsAdmin.checked == true ) {
-            data.cells[2].innerText = "X" 
-        } else{
-            data.cells[2].innerText = "-" 
-        }
-
-    } else {
-
-        // Create new row
-        var newText = document.createTextNode( newUsername.value )
-        var newRow = userTable.insertRow(userTable.rows.length);
-
-        var newCell = newRow.insertCell(0);
-        newCell.appendChild( newText );
-
-        // Append email
-        var newCell = newRow.insertCell(1);
-        var newText = document.createTextNode( newEmail.value )
-        newCell.appendChild( newText );
-
-        // Append admin check
-        var newCell = newRow.insertCell(2);
-        var newText = document.createTextNode( "-" )
-        if (newIsAdmin.checked == true ) {
-            var newText = document.createTextNode( "X" )
-        }
-        newCell.appendChild( newText );
-    }
-
-    // Reset entries to placeholder stuses
-    newUsername.value = newUsername.ariaPlaceholder
-    newEmail.value = newEmail.ariaPlaceholder
-    newIsAdmin.checked = false
-
-}
-
-function emptyListClick() {
-    const userTable = document.getElementById( "users-table" )
-    const rows = document.querySelectorAll( "tr" ).length;
-    
-    for (let ii = 1; ii < rows; ii++ ) {
-        userTable.deleteRow(-1)
     }
 }
 
-function checkExistingUsername( name, table ) {
-    N = table.rows.length
-
-    if ( name === "Username" ) {
-        return [false, 0]
-    } else {
-        for ( ii = 1; ii < N; ii++ ) {
-            oldname = table.rows[ii].cells[0].innerText
-            // console.log(oldname)
-            // console.log(oldname)
-            // console.log(name == oldname)
-            if (name === oldname) {
-                return [true, ii]
-            }
-        }
-    }
-
-    return [false, 0]
-}
-
+window.addEventListener('load', generateTable );
