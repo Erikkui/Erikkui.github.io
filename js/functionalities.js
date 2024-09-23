b208f7012f256ad13dfb6ff9cdb4b90077594e6c
Erikkui <erik.kuitunen@outlook.com>
2024-09-23 22:31:29 +0300

    Completed task 5

--- js/functionalities.js
+++ js/functionalities.js
@@ -1,5 +1,6 @@
-
-
+// Huge thanks to Mohamed Shimer for almost fatherly advice at LTKY's office when doing the first tasks
+// Conditional row color help from this Medium article:
+// https://medium.com/@taylor.stein09/conditional-formatting-with-javascript-and-css-7f5cbd7989e3
 
 async function generateTable() {
 
@@ -25,23 +26,44 @@ async function generateTable() {
     // Add rows to table
     addTableRows( dataTable, municipalitiesArray, populations, employmentArray )
 
-    
-
 }
+
 
 function addTableRows( table, data1, data2, data3 ) {
 
-    for (let ii = 0; ii < data1.length; ii++ ) {
+    // Calculating employment rates
+    employmentRates = data2.map( (elem, i ) => ( 100*data3[i] / elem ).toFixed(2) )
+
+
+    // Adding rows in loop
+    for ( let ii = 0; ii < data1.length; ii++ ) {
         let row = table.getElementsByTagName("tbody")[0].insertRow()
         let municCell = row.insertCell()
         let populCell = row.insertCell()
         let employmentCell = row.insertCell()
+        let emplRateCell = row.insertCell()
 
         municCell.appendChild( document.createTextNode( data1[ii] ) )
         populCell.appendChild( document.createTextNode( data2[ii] ) )
         employmentCell.appendChild( document.createTextNode( data3[ii] ) )
+        emplRateCell.appendChild( document.createTextNode( employmentRates[ii] + "%" ) )
+
+        // Adding a class to the rows with specified emplyment rates for conditional formatting
+        addClass( row, employmentRates[ii] )
         
     }
 }
+
+
+function addClass( elem, percent ) {
+
+    if ( percent > 45  ) {
+        elem.classList.add( "over-45" )
+    } else if ( percent < 25 ) {
+        elem.classList.add( "under-25" )
+    }
+
+}
+
 
 window.addEventListener('load', generateTable );
